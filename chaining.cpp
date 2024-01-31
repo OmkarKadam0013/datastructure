a31ba338d058fcccfeed24c7b77ec085af2df2f9
Omkar Kadam <143085875+OmkarKadam0013@users.noreply.github.com>
2024-01-31 12:53:55 +0530

    Update chaining.cpp

--- chaining.cpp
+++ chaining.cpp
@@ -4,45 +4,51 @@
 #include<algorithm>
 using namespace std;
 class hashing{
-    
-    vector<list<int>> hashtable;
+    vector<list<int>>hashtable;
     int buckets;
     public:
-    hashing(int size){
-        buckets = size;
-        hashtable.resize(buckets);
-    }
-    int hashval(int key){
-        return key%buckets;
-    }
-    void add(int key){
-        int idx = hashval(key);
-        hashtable[idx].push_back(key);
-    }
+   hashing(int size){
+    buckets = size;
+    hashtable.resize(buckets);
+   }
+   int hashval(int key){
+    return key % buckets;
+   }
+   void addKey(int key){
+    int idx = hashval(key);
+    hashtable[idx].push_back(key);
+   }
    list<int>::iterator searchKey(int key){
-        int idx = hashval(key);
-        return find(hashtable[idx].begin(), hashtable[idx].end(), key);
+    int idx = hashval(key);
+    return find(hashtable[idx].begin(),hashtable[idx].end(),key);
+   }
+   void deleteKey(int key){
+    int idx = hashval(key);
+    if(hashtable[idx].end() != searchKey(key)){
+    hashtable[idx].erase(searchKey(key));
     }
-    void deletekey(int key){
-        list<int>::iterator it = searchKey(key);
-        int idx = hashval(key);
-        if(it != hashtable[idx].end()){
-           hashtable[idx].erase(it);
-        }
-        else{
-            cout<<"Key is no present in hash-table"<<endl;
+    else{
+        cout<<"Element is not present !!"<<endl;
+    }
+   }
+   void display(){
+    for(int it = 0 ;it<hashtable.size();it++){
+        for(auto i = hashtable[it].begin();i != hashtable[it].end(); i++){
+            cout<<"elments are "<<*i<<endl;
         }
     }
+   }
+
 };
 int main(){
     hashing h(10);
-    h.add(10);
-    h.add(20);
-    h.add(3);
-    h.deletekey(3);
-    h.deletekey(3);
-    cout<<endl;
-    cout<<"10 is at index : "<<*h.searchKey(10);
-    cout<<endl;
+    h.addKey(10);
+    h.addKey(3);
+    h.addKey(4);
+    h.addKey(11);
+    h.deleteKey(3);
+    h.display();
+   
+
     return 0;
 }
